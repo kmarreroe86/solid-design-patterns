e6aac084b70d47be7c874fd192dc0c1d127cdb59
kmarreroe <karel.marrero@thoughtworks.com>
2023-01-26 17:49:47 -0500

    Solid(SRP, OP)

---
+++ solid-design-patterns/src/main/java/solid/singleresponsability/UserPersistenceService.java
@@ -0,0 +1,11 @@
+package solid.singleresponsability;
+
+//A separate class for handling persistence 
+public class UserPersistenceService {
+
+	private Store store = new Store();
+	
+	public void saveUser(User user) {
+		store.store(user);
+	}
+}
