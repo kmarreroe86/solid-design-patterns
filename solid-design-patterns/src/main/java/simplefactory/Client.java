7346bcfe1dddf6d1166f9c7ca2e7bae61e4a0cf4
Karel <kmarrero@visualaimsoft.com>
2023-01-29 23:06:55 -0500

    Simple factory method

---
+++ solid-design-patterns/src/main/java/simplefactory/Client.java
@@ -0,0 +1,12 @@
+package simplefactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Post post = PostFactory.createPost("blog");
+		System.out.println(post);
+
+	}
+
+}
